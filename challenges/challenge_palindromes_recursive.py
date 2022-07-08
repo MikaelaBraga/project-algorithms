b8a3d8cf8ae2a4bd54eefd16791fbabbebac4f35
MIkaela Braga <bragamikaela@gmail.com>
2022-07-08 20:01:39 -0300

    feat: implement challenge_palindromes_recurcive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # caso base
+    if len(word) == 0:
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+    
+    if low_index < high_index:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return word[low_index] == word[high_index]
