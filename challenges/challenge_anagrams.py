198eb404534a747464dec425fa6ed30b0df8517f
MIkaela Braga <bragamikaela@gmail.com>
2022-07-08 20:48:57 -0300

    fix: fix lint errors

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,14 +1,10 @@
-from typing import Counter
-
 # ref: https://www.delftstack.com/howto/python/python-anagram/
 # https://www.datacamp.com/tutorial/case-conversion-python
-def is_anagram(first_string, second_string):
-    first_lower_string = first_string.lower()
-    second_lower_string = second_string.lower()
 
-    if len(first_string) != len(second_string):
-        return False
-    
+def verify_strings(str1, str2):
+    first_lower_string = str1.lower()
+    second_lower_string = str2.lower()
+
     count_first_string = {}
     count_second_string = {}
 
@@ -17,11 +13,19 @@ def is_anagram(first_string, second_string):
             count_first_string[letter] = 1
         else:
             count_first_string[letter] += 1
-        
+
     for letter in second_lower_string:
         if letter not in count_second_string.keys():
             count_second_string[letter] = 1
         else:
             count_second_string[letter] += 1
-    
+
     return True if count_first_string == count_second_string else False
+
+
+def is_anagram(first_string, second_string):
+
+    if len(first_string) != len(second_string):
+        return False
+
+    return verify_strings(first_string, second_string)
