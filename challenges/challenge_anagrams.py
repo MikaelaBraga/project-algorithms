58d8f9636f909958aaaf184aa7d414ab9212bd8b
MIkaela Braga <bragamikaela@gmail.com>
2022-07-08 20:30:48 -0300

    feat: implement challenge_anagrams funtion

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,27 @@
+from typing import Counter
+
+# ref: https://www.delftstack.com/howto/python/python-anagram/
+# https://www.datacamp.com/tutorial/case-conversion-python
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_lower_string = first_string.lower()
+    second_lower_string = second_string.lower()
+
+    if len(first_string) != len(second_string):
+        return False
+    
+    count_first_string = {}
+    count_second_string = {}
+
+    for letter in first_lower_string:
+        if letter not in count_first_string.keys():
+            count_first_string[letter] = 1
+        else:
+            count_first_string[letter] += 1
+        
+    for letter in second_lower_string:
+        if letter not in count_second_string.keys():
+            count_second_string[letter] = 1
+        else:
+            count_second_string[letter] += 1
+    
+    return True if count_first_string == count_second_string else False
