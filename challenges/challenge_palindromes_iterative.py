849f34b05aa3d3449561a48ec9181229aa3a9c4f
MIkaela Braga <bragamikaela@gmail.com>
2022-07-29 21:52:39 -0300

    feat: implement is_palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    string_size = len(word)
+    
+    if string_size == 0:
+        return False
+    
+    for i in range(string_size // 2):
+        if word[i] != word[string_size - i - 1]:
+            return False
+    
+    return True
